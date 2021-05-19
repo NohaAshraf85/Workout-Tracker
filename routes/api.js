9e009efe27e79c1d0468ad2fcd417945d82f0b8b
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-05-19 02:15:51 -0400

    created the apiroutes and adjusted the server.js, tested and everything is working

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-const Transaction = require("../models");
+const Workout = require("../models/workout.js");
 
-router.post("/api/workout", ({ body }, res) => {
+router.post("/api/workouts", ({ body }, res) => {
     Workout.create(body)
       .then(dbWorkout => {
         res.json(dbWorkout);
@@ -11,12 +11,73 @@ router.post("/api/workout", ({ body }, res) => {
       });
   });
 
-  router.post("/api/workout/bulk", ({ body }, res) => {
-    Transaction.insertMany(body)
-      .then(dbTransaction => {
-        res.json(dbTransaction);
+//   router.put("/api/workouts", ({ body }, res) => {
+//     Workout.create(body)
+//       .then(dbWorkout => {
+//         res.json(dbWorkout);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
+//   });
+
+  router.put('/api/workouts/:id', async (req, res) => {
+
+    console.log("From Put:"+req.params.id);
+
+    Workout.updateOne(
+        { _id: req.params.id },
+        { $push: { exercises: req.body } },
+        (error, edited) => {
+          if (error) {
+            console.log(error);
+            res.send(error);
+          } else {
+            console.log(edited);
+            res.send(edited);
+          }
+        }
+      )
+
+    
+   
+  });
+
+  router.get("/api/workouts", (req, res) => {
+    Workout.find({})
+      .sort({ date: -1 })
+      .then(dbWorkout => {
+        res.json(dbWorkout);
       })
       .catch(err => {
         res.status(400).json(err);
       });
   });
+  router.get("/api/workouts/range", (req, res) => {
+
+    Workout.aggregate([
+        { $addFields: {
+          totalDuration: {
+            $sum: "$exercises.duration"
+          }
+        }}
+      ]).sort( {
+        _id: -1
+      })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+          })
+          .catch(err => {
+            res.status(400).json(err);
+          });
+});
+
+  router.get('/api/workouts/:id', async (req, res) => {
+    // find one category by its `id` value
+    Workout.findById(req.params.id).then(function(workoutData) {
+        res.json(workoutData);
+      });
+  });
+
+ 
+  module.exports = router;
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+                                                                                       
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false  
 });
 
 // routes
