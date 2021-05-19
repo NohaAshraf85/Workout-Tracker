9f0a122896f1571c6655e646946916efa400fcf8
NohaAshraf85 <noha_ashraf85@hotmail.com>
2021-05-19 11:50:49 -0400

    version 3 of the README, not yet final, fixed an issue where the duration was not calculated

--- README.md
+++ README.md
@@ -42,3 +42,52 @@ To install the application locally, follow the following steps:
     * [express](https://www.npmjs.com/package/express) through `npm i express` command
     * [mongoose](https://www.npmjs.com/package/mongoose) thgouth `npm i mongoose` command
     * [morgan](https://www.npmjs.com/package/morgan) through `npm i morgan` command
+4. Make sure that the MongoDB is running, through running the command `mongo` either on gitbash, command line or powershell
+5. Seed the Database using the command `npm seed` or `node seeders/seed.js`
+6. Run the local server using the command `npm start` or `node server.js`
+7. To open the application locally, open localhost:3000 in your browser
+
+## Usage
+The Workout Tracker enables the user to track their daily exercises through adding different types of workouts and being able to track the overall progress through the dashboard providing the user with workout stats.
+
+## How It Works
+The Workout Tracker application is deployed on Heroku through the following link [Workout-Tracker](https://stormy-escarpment-39241.herokuapp.com/?id=60a4b3ced1c0fc00154aee1e) while the database is setup on MondoDB Atlas.
+
+Upon opening the deployed application the user is presented with the Homepage which enables the user to create a new Workout. 
+
+![Homepage](./assets/images/Homepage.png)
+
+**New Workout:** chosing to create a new workout presents the user with the option to add the type of exercise form a drop-down list, the list currently includes Resistance and Cardio workouts
+
+![workoutList](./assets/images/workoutList.png)
+
+1. **Resistance:** 
+The Resistance exercises provides the user with an interface that includes a form to be filled out with the workout details, Exercise Name, Weight in lbs, Sets, Reps, Duration in minutes.
+
+![resistance](./assets/images/resistance.png)
+
+Upon filling all the required workout details, the user can either choose to add the exercise and then click on complete. 
+
+Once the user is done adding the exercise, a page displaying the Last workout is rendered, presenting the details 
+
+
+## License
+![APM](https://img.shields.io/apm/l/README)
+
+## Badges
+
+![HTML](https://img.shields.io/badge/HTML-blue)
+![Bootstrap](https://img.shields.io/badge/Bootstrap-blue)
+![CSS](https://img.shields.io/badge/CSS-blue)
+
+## Contributing 
+
+To contribute to this project, please make sure you follow the guidelines in [The Contributor Covenant](https://www.contributor-covenant.org/) as general guidelines.
+Kindly maintain the highest ethics and respect. For further questions or requests kindly contact me through my [Email](mailto:noha_ashraf85@hotmail.com) or [GitHub](https://github.com/NohaAshraf85).
+
+## Tests
+The application has been tested on the browser and on multiple devices to test the responsiveness.
+
+## Questions
+For any questions, please contact me through [GitHub](https://github.com/NohaAshraf85) 
+or [Email](mailto:noha_ashraf85@hotmail.com)
--- routes/api.js
+++ routes/api.js
@@ -1,83 +1,59 @@
+const db = require("../models");
 const router = require("express").Router();
-const Workout = require("../models/workout.js");
 
-router.post("/api/workouts", ({ body }, res) => {
-    Workout.create(body)
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-
-//   router.put("/api/workouts", ({ body }, res) => {
-//     Workout.create(body)
-//       .then(dbWorkout => {
-//         res.json(dbWorkout);
-//       })
-//       .catch(err => {
-//         res.status(400).json(err);
-//       });
-//   });
-
-  router.put('/api/workouts/:id', async (req, res) => {
-
-    console.log("From Put:"+req.params.id);
-
-    Workout.updateOne(
-        { _id: req.params.id },
-        { $push: { exercises: req.body } },
-        (error, edited) => {
-          if (error) {
-            console.log(error);
-            res.send(error);
-          } else {
-            console.log(edited);
-            res.send(edited);
-          }
-        }
-      )
-
-    
-   
-  });
-
-  router.get("/api/workouts", (req, res) => {
-    Workout.find({})
-      .sort({ date: -1 })
-      .then(dbWorkout => {
-        res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-  });
-  router.get("/api/workouts/range", (req, res) => {
-
-    Workout.aggregate([
-        { $addFields: {
-          totalDuration: {
-            $sum: "$exercises.duration"
-          }
-        }}
-      ]).sort( {
-        _id: -1
-      })
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-          })
-          .catch(err => {
-            res.status(400).json(err);
-          });
+router.post("/api/workouts", (req, res) => {
+  db.Workout.create(req.body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
 });
 
-  router.get('/api/workouts/:id', async (req, res) => {
-    // find one category by its `id` value
-    Workout.findById(req.params.id).then(function(workoutData) {
-        res.json(workoutData);
-      });
-  });
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.updateOne(
+    {_id: req.params.id}, { $push: {exercises: req.body}})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
- 
-  module.exports = router;
+router.get("/api/workouts", (req, res) => {
+  db.Workout.aggregate([
+    { $addFields: {
+      totalDuration: {
+        $sum: "$exercises.duration"
+      }
+    }}
+  ]).then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.aggregate([
+    { $addFields: {
+      totalDuration: {
+        $sum: "$exercises.duration"
+      }
+    }}
+  ]).sort( {
+    _id: -1
+  }).limit(10)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+});
+
+
+module.exports = router;
